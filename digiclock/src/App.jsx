e4de8cca695d16e20ab60db6bf2b2499bec2559d
Balsher Sran <balshersran@Balshers-MacBook-Pro.local>
2024-12-27 00:00:44 -0800

    added month/day/year

--- digiclock/src/App.jsx
+++ digiclock/src/App.jsx
@@ -1,28 +1,16 @@
-
 import Clock from "./Components/clock";
 import "./index.css";
 
-function App() {
+function App(q) {
   return (
     <>
-      <div className="container mw-auto px-4 height-auto flex flex-col justify-center items-center align-center">
-        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-teal-200">
+      <div className="container mw-auto p-48 height-auto">
+        <div className="max-w-sm rounded overflow-hidden shadow-2xl bg-teal-300">
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2 text-center">digiClock</div>
-              <p>
-              <Clock/>
-              </p>
-          </div>
-          <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #month
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #day
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #year
-            </span>
+            <p>
+              <Clock />
+            </p>
           </div>
         </div>
       </div>
--- digiclock/src/components/Clock.jsx
+++ digiclock/src/components/Clock.jsx
@@ -1,27 +1,52 @@
 import { useState, useEffect } from "react";
 
 export default function Clock() {
-    const [time , setTime] = useState(new Date());
+  const [time, setTime] = useState(new Date());
 
-    useEffect(() => {
-        const interval = setInterval(()=> {
-            setTime(new Date());
-        }, 1000);
-        return () => clearInterval(interval);
-    }, [])
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
-    let hour = time.getHours();
-    let minutes = time.getMinutes();
-    let seconds = time.getSeconds();
+  let hour = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+  let month = time.getMonth();
+  let day = time.getDate();
+  let year = time.getFullYear();
 
-    return (
-        <>
-        <div className="flex flex-row justify-center">
+  return (
+    <>
+      <div className="flex flex-row justify-center">
         <span className="p-4 text-4xl">{hour}</span>
         <span className="p-4">:</span>
         <span className="p-4 text-4xl">{minutes}</span>
         <span className="p-4">{seconds}</span>
-        </div>
-        </>
-    );
+      </div>
+      <div className="px-6 pt-4 pb-2 text-center">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          Month
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          Day
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          Year
+        </span>
+      </div>
+      <div className="px-6 pt-4 pb-2 text-center">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          {month}
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          {day}
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          {year}
+        </span>
+      </div>
+    </>
+  );
 }
